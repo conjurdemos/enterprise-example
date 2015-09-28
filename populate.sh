ea87b16fa9e42c1511534e6e3bd9e79fe3677d9a
mike d. kail <mdkail@gmail.com>
2015-09-28 10:53:30 -0700

    Updated for Minimum Viable Demo story (HR Admin app)

--- populate.sh
+++ populate.sh
@@ -1,21 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+#
 
-set -x
+conjur script execute --as-group security_admin global-secrets.rb
 
-$(pwd)/populate.exp
-
-conjur group members add --admin operations mindy.fredricks
-conjur group members add         operations dave.bartell
-
-conjur group members add --admin hr-admins carlos.vicente
-conjur group members add         hr-admins jill.blair
-
-conjur group members add --admin employees russ.reed
-conjur group members add         employees jason.knight
-conjur group members add         employees jim.harper
-conjur group members add         employees cyndi.bruens
-conjur group members add         employees barbara.lazar
-conjur group members add         employees sam.goodman
-conjur group members add         employees jeff.slater
-conjur group members add         employees sharon.foster
-conjur group members add         employees dick.evans
+conjur script execute --as-group security_admin users.rb
