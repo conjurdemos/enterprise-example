dcd5d4cbe0e3f759594c84c6b5610dbb1fa1e578
Dustin Collins <dustinmm80@users.noreply.github.com>
2015-09-29 11:38:02 -0400

    Fix name of policy

--- populate.sh
+++ populate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -e
 #
 
-conjur script execute --as-group security_admin global-secrets.rb
+conjur script execute --as-group security_admin secrets.rb
 
 conjur script execute --as-group security_admin users.rb
