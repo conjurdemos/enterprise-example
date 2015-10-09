fc854b76b5ce23bb7c9777f37be736bc1142c8ca
Kurt Stutsman <kstutsman@conjur.net>
2015-10-09 08:49:40 -0500

    Load users from names.txt and adjust perms
    Make all users employees and add some additional operators to be more realistic.

--- users.rb
+++ users.rb
@@ -1,7 +1,12 @@
-%w(june.riley riekje.tieman steven.nelson julius.autio angel.ruiz marian.tucker clara.philippe john.chapman noah.wilson rasmus.kauppi katarina.meij mindy.fredricks dave.bartell carlos.vicente jill.blair russ.reed jason.knight).each do |login|
-  user login, ownerid: api.group('security_admin').roleid, password: "password"
-end
+cwd = File.dirname(__FILE__)
+logins = File.open("#{cwd}/names.txt", "r:UTF-8").readlines.map! { |x| x.chomp! }
 
+logins.each do |login|
+  login.chomp!
+  if !login.empty? then
+    user login, ownerid: api.group('security_admin').roleid, password: "password"
+  end   
+end
 
 group '/hradmins' do
   add_member user('june.riley'), admin_option: true
@@ -9,19 +14,17 @@ group '/hradmins' do
 end
 
 group '/employees' do
-  add_member user('steven.nelson')
-  add_member user('julius.autio')
-  add_member user('angel.ruiz')
-  add_member user('marian.tucker')
-  add_member user('clara.phillipe')
-  add_member user('john.chapman')
-  add_member user('noah.wilson')
-  add_member user('rasmus.kauppi')
-  add_member user('katarina.meij')
-  add_member user('jill.blair')
+  logins.each { |login| add_member user(login) }
 end
 
 group '/operations' do
   add_member user('russ.reed'), admin_option: true
   add_member user('jason.knight'), admin_option: true
+  add_member user('kyle.wheeler'), admin_option: true
+  add_member user('marin.dubois'), admin_option: true
+  add_member user('carol.rodriquez'), admin_option: true
+  add_member user('karen.wood'), admin_option: true
+  add_member user('caroline.mccoy'), admin_option: true
+  add_member user('june.riley'), admin_option: true
+  add_member user('riekje.tieman'), admin_option: true
 end
