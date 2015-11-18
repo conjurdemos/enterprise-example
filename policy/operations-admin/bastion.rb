099875a0b5752b0ff1eeaa191c9ec4df95a29f1b
Daniel Warner <dan@conjur.net>
2015-11-18 17:22:33 -0500

    bastion policy text changes

--- policy/operations-admin/bastion.rb
+++ policy/operations-admin/bastion.rb
@@ -1,12 +1,12 @@
 policy "bastion/v1" do
   policy_resource.annotations['description'] = 'Policy to restrict access to bastion hosts'
   admins = group "admins"
-  admins.resource.annotations['description'] = "admins group has ssh access to the bastion hosts with elevated privilege"
+  admins.resource.annotations['description'] = "This group has admin privilege on the bastion hosts in the prod/bastion/v1 layer"
   users  = group "users"
-  users.resource.annotations['description'] = "users group has ssh access to the bastion hosts with user privilege"
+  users.resource.annotations['description'] = "This group has user-level privilege on the bastion hosts in the prod/bastion/v1 layer"
   
   layer do |l|
-  	layer.resource.annotations['description'] = "bastion hosts will be added to this layer to restrict access"
+  	layer.resource.annotations['description'] = "This layer is designated for internet-facing machines that accept external connections"
     add_member "use_host", users
     add_member "admin_host", admins
     
