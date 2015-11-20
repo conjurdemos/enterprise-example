d4f4f5a60fb783ed1e0f1a06a8f90851573ae919
Dustin Byrne <dbyrne@conjur.net>
2015-11-20 13:17:05 -0500

    adding secret managers group to jenkins teams

--- policy/ci-admin/team-b.rb
+++ policy/ci-admin/team-b.rb
@@ -3,9 +3,19 @@ policy "jenkins/team-b" do
   policy_resource.annotations['description'] = 'This policy declares secrets (via Conjur variables) which are available to Jenkins jobs located within the team-b Jenkins Folder.'
   variables = [
     [variable('cloud/access_key_id'), "team-b Jenkins job api key to cloud service"],
-    [variable('cloud/secret_access_key'),"team-b Jenkins job secret access key"]
+    [variable('cloud/secret_access_key'),"team-b Jenkins job secret access key"],
+    [variable('rsa_key'),"RSA key to authenticate with an external service"]
   ]
 
+  group "secret_managers" do |group|
+    variables.each {|var| 
+      can 'read', var[0]
+      can 'execute', var[0]
+      can 'update', var[0]
+      var[0].resource.annotations['description'] = var[1]
+    }
+  end
+  
   host do |host|    
     host.resource.annotations['kind'] = "Jenkins folder"
     host.resource.annotations['description'] = "Host identity for running Jenkins jobs in team-b folder - can access team-b API keys"    
