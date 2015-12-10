34ccad8bc6ef7da25facb2a4c71a31e45b5e24b2
Dustin Byrne <dbyrne@conjur.net>
2015-12-10 14:22:23 -0500

    remove references to 'team-b'

--- policy/ci_admin/admin_ui.rb
+++ policy/ci_admin/admin_ui.rb
@@ -1,10 +1,10 @@
 # The host role in this policy will be used by the jobs in a folder called "".
 policy "jenkins/admin_ui/v1" do
-  policy_resource.annotations['description'] = 'This policy declares secrets (via Conjur variables) which are available to Jenkins jobs located within the team-b Jenkins Folder.'
+  policy_resource.annotations['description'] = 'This policy declares secrets (via Conjur variables) which are available to Jenkins jobs located within the admin_ui Jenkins Folder.'
   
   variables = [
-    [variable('cloud/access_key_id'), "team-b Jenkins job api key to cloud service"],
-    [variable('cloud/secret_access_key'),"team-b Jenkins job secret access key"],
+    [variable('cloud/access_key_id'), "admin_ui Jenkins job api key to cloud service"],
+    [variable('cloud/secret_access_key'),"admin_ui Jenkins job secret access key"],
     [variable('rsa_key'),"RSA key to authenticate with an external service"]
   ]
 
@@ -19,7 +19,7 @@ policy "jenkins/admin_ui/v1" do
 
   host do |host|    
     host.resource.annotations['kind'] = "Jenkins folder"
-    host.resource.annotations['description'] = "Host identity for running Jenkins jobs in team-b folder - can access team-b API keys"    
+    host.resource.annotations['description'] = "Host identity for running Jenkins jobs in admin_ui folder - can access admin_ui API keys"    
     variables.each {|var| 
       can 'read', var[0]
       can 'execute', var[0]
