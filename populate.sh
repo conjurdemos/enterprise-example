a21796ce9b506520166d3caa7f736c148d636ab5
Jon Mason <jonathan.j.mason@gmail.com>
2016-02-05 12:14:53 -0600

    Save API keys from populate.sh.
    Because the context files have different formats in the old and new dsl,
    the `script execute` commands save their contexts to `conjur.json`, while
    the `policy2 load` commands save theirs to `api-keys.json`.  This reflects
    the fact that the new dsl simply writes the api keys for roles it creates
    as a hash of `roleid => api_key`, whereas the old dsl writes additional information such as certs and an appliance url.

--- populate.sh
+++ populate.sh
@@ -2,16 +2,16 @@
 
 mkdir -p tmp
 
-conjur script execute --as-group security_admin policy/groups.rb
-conjur script execute --as-group security_admin policy/users.rb
+conjur script execute --context conjur.json --as-group security_admin policy/groups.rb
+conjur script execute --context conjur.json --as-group security_admin policy/users.rb
 
 for script in $(find policy/* -name "*.yml"); do
 	read folder group file <<< $(echo $script | tr "/" " ")
 	if [ ! -z "$file" ]; then
 		echo Loading policy $file as group $group
-		conjur policy2 load --namespace prod --as-group $group $folder/$group/$file
+		conjur policy2 load --context api-keys.json --namespace prod --as-group $group $folder/$group/$file
 	fi
 done        	
 
-conjur policy2 load --as-group security_admin policy/global_records.yml
-conjur policy2 load --as-group security_admin policy/entitlements.yml
+conjur policy2 load --context api-keys.json --as-group security_admin policy/global_records.yml
+conjur policy2 load --context api-keys.json --as-group security_admin policy/entitlements.yml
