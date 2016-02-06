5605e7253a6f4180aefd6b1671ccec49be57c09a
Josh Bregman <jbregman@gmail.com>
2016-02-06 11:49:30 -0500

    Basic Client Working

---
+++ launcher/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=$(awk '/^aws_access_key_id/{print $3}' ~/.aws/credentials)
+export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret_access_key/{print $3}' ~/.aws/credentials)
+export AWS_KEYPAIR_NAME=launch-conjur-demo-$HOSTNAME
+export SSH_PRIVATE_KEY_PATH=id_$AWS_KEYPAIR_NAME  
+aws ec2 create-key-pair --key-name $AWS_KEYPAIR_NAME --query 'KeyMaterial' --output text> $SSH_PRIVATE_KEY_PATH
+chmod 400 $SSH_PRIVATE_KEY_PATH
+export CONJUR_ADMIN_PASSWORD=$(openssl rand -hex 8) 
+export AWS_BASE_AMI=ami-35b6ca5f
+
+echo "Admin password is : $CONJUR_ADMIN_PASSWORD"
+
+vagrant up --provider aws --debug
