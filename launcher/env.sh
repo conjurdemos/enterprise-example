18599971a1fb6d328aa2a94b478f807815ee81d2
Josh Bregman <joshbregman@Joshs-MacBook-Air.local>
2016-02-12 09:21:43 -0500

    Tweaked Makefile to keep server running

---
+++ launcher/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=$(awk '/^aws_access_key_id/{print $3}' ~/.aws/credentials)
+export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret_access_key/{print $3}' ~/.aws/credentials)
+export AWS_KEYPAIR_NAME=launch-conjur-demo-$HOSTNAME
+export SSH_PRIVATE_KEY_PATH=id_$AWS_KEYPAIR_NAME
+export AWS_BASE_AMI=ami-35b6ca5f
