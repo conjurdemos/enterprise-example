dcad595b82478752d19a21188da4737bb8eb322d
Josh Bregman <jbregman@gmail.com>
2016-02-22 22:11:28 +0000

    Changed to 4.6.1 appliance

--- launcher/env.sh
+++ launcher/env.sh
@@ -4,4 +4,4 @@ export AWS_ACCESS_KEY_ID=$(awk '/^aws_access_key_id/{print $3}' ~/.aws/credentia
 export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret_access_key/{print $3}' ~/.aws/credentials)
 export AWS_KEYPAIR_NAME=launch-conjur-demo-$HOSTNAME
 export SSH_PRIVATE_KEY_PATH=id_$AWS_KEYPAIR_NAME
-export AWS_BASE_AMI=ami-35b6ca5f
+export AWS_BASE_AMI=ami-4dd1e727
